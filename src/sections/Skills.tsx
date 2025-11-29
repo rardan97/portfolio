d9325ea8f9db29f831a73ca1f6a7dc474ecb17a0
rardan97 <risalardan@gmail.com>
2025-11-30 06:42:11 +0700

    update project

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -22,12 +22,12 @@ const Skills = () => {
 
   return (
     <section id="skills" className="py-30 flex flex-col bg-gray-100 dark:bg-slate-950  items-center justify-center xl:px-30 lg:px-20 md:px-20 sm:px-5 max-w-full mx-auto">
-      <h2 className="text-4xl font-bold text-center">My Skills</h2>
+      <h2 className="text-4xl font-bold text-center">Skills</h2>
       <p className="text-center px-10 text-gray-600 dark:text-gray-300 mb-12 max-w-xl mx-auto">
         These are some of the technologies I regularly use to build projects.
       </p>
 
-      <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-6 px-20 gap-6 w-full">
+      <div className="px-10 md:px-10 sm:px-20 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-6  gap-6 w-full">
         {skills.map((skill: Skill, index) => (
           <SkillComponent
             key={index}
